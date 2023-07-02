213739bb952c76ed1463f527fee66a0018002adf
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-07-02 21:51:49 +0700

    show image and use TouchableHighlight

--- lib/App.tsx
+++ lib/App.tsx
@@ -1,5 +1,14 @@
 import {Component, ReactNode} from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  Alert,
+  Button,
+  TouchableHighlight,
+} from 'react-native';
 
 type state = {
   inputText: string;
@@ -13,15 +22,25 @@ class App extends Component<{}, state> {
     };
   }
 
+  clickMe() {
+    Alert.alert('Thanks');
+  }
+  longClickMe() {
+    Alert.alert('Long press');
+  }
+
   render(): ReactNode {
     return (
       <View style={styles.container}>
-        <Text>{this.state.inputText}</Text>
-        <TextInput
-          style={styles.inputText}
-          value={this.state.inputText}
-          onChangeText={(text) => this.setState({inputText: text})} 
-        />
+        <TouchableHighlight
+          onPress={this.clickMe}
+          underlayColor={'gray'}
+          onLongPress={this.longClickMe}>
+          <Image
+            style={{width: 300, height: 100, resizeMode: 'center'}}
+            source={require('../images/pic.jpg')}
+          />
+        </TouchableHighlight>
       </View>
     );
   }
@@ -34,14 +53,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#F352C0',
     // justifyContent: 'center',
     alignItems: 'center',
-  },
-  inputText: {
-    minWidth: 100,
-    maxWidth: 300,
-    borderRadius: 6,
-    borderColor: 'black',
-    borderWidth: 1,
-    borderStyle: 'solid',
   },
 });
 
